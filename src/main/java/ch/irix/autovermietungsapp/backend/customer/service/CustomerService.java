0149a3cdda822d87307705f1f1a3a32b256ebc0b
YannickZutter <yannick.zutter@irix.ch>
2023-05-12 14:50:42 +0200

    asdf

--- src/main/java/ch/irix/autovermietungsapp/backend/customer/service/CustomerService.java
+++ src/main/java/ch/irix/autovermietungsapp/backend/customer/service/CustomerService.java
@@ -1,16 +1,12 @@
 package ch.irix.autovermietungsapp.backend.customer.service;
 
 import ch.irix.autovermietungsapp.backend.customer.model.Customer;
-import ch.irix.autovermietungsapp.backend.customer.repository.CustomerRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 
-public interface CustomerService impl{
+public interface CustomerService{
 
     public List<Customer> getAllCustomers();
 
